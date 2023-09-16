02dab2cb7d41a67f39524e49d7b32db334067a72
thigithub <thiagosantosdba@gmail.com>
2023-09-16 15:28:07 -0300

    codigo atualizado

--- MeuProjeto/try_except_fiap.py
+++ MeuProjeto/try_except_fiap.py
@@ -1,10 +1,19 @@
+class IdadeMaximaExcedida(Exception):
+    def __str__(self):
+        return ('A idade não pode ser superior a 125 anos')
+
 try:
     idade = int(input('Por favor, insira a idade do aluno: '))
-    idade_amigo = input('Por favor, forneça a idade do amigo: ')
+    idade_amigo = int(input('Por favor, forneça a idade do amigo: '))
 
     idade_total = idade + idade_amigo
-except ValueError:
-    print('Por favor entre com um valor inteiro para idade')
+    if idade < 0 or idade_amigo < 0:
+        raise  ValueError('Valor da idade não pode ser menor que zero')
+    elif idade > 125 or idade_amigo > 125:
+        raise IdadeMaximaExcedida
+except ValueError as Error:
+    #print('Por favor entre com um valor inteiro para idade')
+        print('Erro', Error)
 except TypeError:
     print('Estamos com problemas tecnico, tentando somar inteiro com string. Aguarde a correção do código.')
 else:
